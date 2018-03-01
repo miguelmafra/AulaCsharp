a5ec3805d3d776d1cc80c1755c8dce29fe319d39
1713606 <1713606@DCIC116959>
2018-02-28 21:57:25 -0300

    Adicionando a classe Pessoa

---
+++ SitemaEntregas/Modelos/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace Modelos
+{
+    class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+
+    }
+}
