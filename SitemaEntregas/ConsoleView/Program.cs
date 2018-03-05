d5789edbfa02b76d282dfe538157d4d461752ebb
Miguel Mafra Lorenzi <miguelml159357@gmail.com>
2018-03-05 20:27:05 -0300

    Criando a versão inicial do console

--- SitemaEntregas/ConsoleView/Program.cs
+++ SitemaEntregas/ConsoleView/Program.cs
@@ -1,12 +1,47 @@
 ﻿using Modelos;
+using System;
 
 namespace ConsoleView
 {
     class Program
     {
+        private static int Menu()
+        {
+            Console.WriteLine("Escolha sua opção");
+            Console.WriteLine(" - Clientes - ");
+            Console.WriteLine("1 - Cadastrar Novo ");
+            Console.WriteLine("2 - Pesquisar Cliente ");
+            Console.WriteLine("3 - Editar Cliente ");
+
+            Console.WriteLine(" - Geral - ");
+            Console.WriteLine("4 - Limpar Tela");
+            Console.WriteLine("5 - Sair ");
+
+            //return Convert.ToInt32(Console.ReadLine());
+            return int.Parse(Console.ReadLine());
+        }
         static void Main(string[] args)
         {
-            Pessoa p = new Pessoa();
+            int opcaodigitada = Menu();
+
+            switch (opcaodigitada)
+            {
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+
+                default:
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
