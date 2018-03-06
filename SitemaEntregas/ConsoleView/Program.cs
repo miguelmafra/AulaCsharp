4ad495467779aca7fb3d7170e296f6762be1a0b7
Miguel Mafra Lorenzi <miguelml159357@gmail.com>
2018-03-05 21:36:55 -0300

    Criação de enum OpcoesMenuPrincipal, adicionando switch com OpcoesMenuPrincipal, adicionando métodos: CadastrarCliente e PesquisarCliente

--- SitemaEntregas/ConsoleView/Program.cs
+++ SitemaEntregas/ConsoleView/Program.cs
@@ -5,43 +5,95 @@ namespace ConsoleView
 {
     class Program
     {
-        private static int Menu()
+
+        enum OpcoesMenuPrincipal
+        {
+            CadastrarCliente = 1,
+            PesquisarCliente = 2,
+            EditarCliente = 3,
+            ExluirCliente = 4,
+            LimparTela = 5,
+            Sair = 6
+        }
+
+
+
+
+
+        private static OpcoesMenuPrincipal Menu()
         {
             Console.WriteLine("Escolha sua opção");
             Console.WriteLine(" - Clientes - ");
             Console.WriteLine("1 - Cadastrar Novo ");
             Console.WriteLine("2 - Pesquisar Cliente ");
             Console.WriteLine("3 - Editar Cliente ");
+            Console.WriteLine("4 - Exluir Cliente");
 
             Console.WriteLine(" - Geral - ");
-            Console.WriteLine("4 - Limpar Tela");
-            Console.WriteLine("5 - Sair ");
+            Console.WriteLine("5 - Limpar Tela");
+            Console.WriteLine("6 - Sair ");
 
+
+            string opcao = Console.ReadLine();
+            return (OpcoesMenuPrincipal)int.Parse(opcao);
+            //     ------Caso for int-----
             //return Convert.ToInt32(Console.ReadLine());
-            return int.Parse(Console.ReadLine());
+            //return int.Parse(Console.ReadLine());
         }
         static void Main(string[] args)
         {
-            int opcaodigitada = Menu();
+            OpcoesMenuPrincipal opcaoDigitada = OpcoesMenuPrincipal.Sair;
 
-            switch (opcaodigitada)
+            //     ------Caso for int-----
+            //int opcaoDigitada = 5;
+            do
             {
-                case 1:
+               opcaoDigitada = Menu();
 
-                    break;
-                case 2:
-                    break;
+                switch (opcaoDigitada)
+                {
+                    case OpcoesMenuPrincipal.CadastrarCliente:
+                        break;
+                    case OpcoesMenuPrincipal.PesquisarCliente:
+                        break;
+                    case OpcoesMenuPrincipal.EditarCliente:
+                        break;
+                    case OpcoesMenuPrincipal.ExluirCliente:
+                        break;
+                    case OpcoesMenuPrincipal.LimparTela:
+                        break;
+                    case OpcoesMenuPrincipal.Sair:
+                        break;
+                    default:
+                        break;
+                }
+          
+            } while (opcaoDigitada != OpcoesMenuPrincipal.Sair);
 
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-
-                default:
-                    break;
-            }
-            Console.ReadKey();
+          
         }
+
+        //Metodo Cliente
+        private static Cliente CadastrarCliente()
+        {
+            Cliente cli = new Cliente();
+            Console.Write("Digite o nome: ");
+            cli.Nome = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Digite o CPF:  ");
+            cli.Cpf = Console.ReadLine();
+
+            //...Endereço
+
+            return cli;
+
+        }
+    }
+        private static Cliente PesquisarCliente()
+    {
+        //TODO : Fazer depois
+        return new Cliente();
     }
 }
