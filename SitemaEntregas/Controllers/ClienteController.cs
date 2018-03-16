78f2e1da5afb0d8b3d65720f66b5327ead072040
Miguel Lorenzi <miguelml159357@gmail.com>
2018-03-16 14:13:58 -0300

    Adicionando os métodos SalvarCliente, PesquisarPorNome, PesquisarPorId, Excluir Cliente e ListarClientes

--- SitemaEntregas/Controllers/ClienteController.cs
+++ SitemaEntregas/Controllers/ClienteController.cs
@@ -1,14 +1,59 @@
 ﻿using Modelos;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Controllers
-
-  
 {
     public class ClienteController
     {
+        static List<Cliente> MeusClientes = new List<Cliente>();
+        static int ultimoID = 0;
+
         public void SalvarCliente(Cliente cliente)
         {
-            //TODO: Persistir os dados do cliente
+            int id = ultimoID + 1;
+            ultimoID = id;
+
+            cliente.PessoaId = id;
+            MeusClientes.Add(cliente);
+        }
+
+        public Cliente PesquisarPorNome(string nome)
+        {
+            var c = from x in MeusClientes
+                    where x.Nome.ToLower().Contains(nome.Trim().ToLower())
+                    select x;
+
+            if (c != null)
+                return c.FirstOrDefault();
+            else
+                return null;
+        }
+
+        public Cliente PesquisarPorID(int idCliente)
+        {
+            var c = from x in MeusClientes
+                    where x.PessoaId.Equals(idCliente)
+                    select x;
+
+            if (c != null)
+                return c.FirstOrDefault();
+            else
+                return null;
+        }
+
+        public void ExcluirCliente(int idCliente)
+        {
+            Cliente cli = PesquisarPorID(idCliente);
+
+            if (cli != null)
+                MeusClientes.Remove(cli);
+        }
+
+        public List<Cliente> ListarClientes()
+        {
+            return MeusClientes;
         }
     }
 }
+
