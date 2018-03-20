9548e4165d17a5e2dfa529abe4bb21a7c956af6e
1713606 <1713606@DCIC116950>
2018-03-19 22:13:19 -0300

    Alterando erros

--- SitemaEntregas/ConsoleView/Program.cs
+++ SitemaEntregas/ConsoleView/Program.cs
@@ -7,47 +7,43 @@ namespace ConsoleView
 {
     class Program
     {
-
         enum OpcoesMenuPrincipal
+
         {
             CadastrarCliente = 1,
             PesquisarCliente = 2,
-            EditarCliente = 3,
-            ExluirCliente = 4,
-            LimparTela = 5,
-            Sair = 6
+            ListarClientesCadastrados = 3,
+            EditarCliente = 4,
+            ExcluirCliente = 5,
+            LimparTela = 6,
+            Sair = 7
         }
-
-
-
-
 
         private static OpcoesMenuPrincipal Menu()
         {
-            Console.WriteLine("Escolha sua opção");
+            Console.WriteLine("Escolha sua opcao");
+            Console.WriteLine("");
+
             Console.WriteLine(" - Clientes - ");
-            Console.WriteLine("1 - Cadastrar Novo ");
-            Console.WriteLine("2 - Pesquisar Cliente ");
-            Console.WriteLine("3 - Editar Cliente ");
-            Console.WriteLine("4 - Exluir Cliente");
+            Console.WriteLine("1 - Cadastrar Novo");
+            Console.WriteLine("2 - Pesquisar Cliente");
+            Console.WriteLine("3 - Listar Clientes Cadastrados");
+            Console.WriteLine("4 - Editar Cliente");
+            Console.WriteLine("5 - Excluir Cliente");
 
-            Console.WriteLine(" - Geral - ");
-            Console.WriteLine("5 - Limpar Tela");
-            Console.WriteLine("6 - Sair ");
+            Console.WriteLine(" - Geral -");
+            Console.WriteLine("6 - Limpar Tela");
+            Console.WriteLine("7 - Sair");
 
-
+            //return Convert.ToInt32(Console.ReadLine());
             string opcao = Console.ReadLine();
             return (OpcoesMenuPrincipal)int.Parse(opcao);
-            //     ------Caso for int-----
-            //return Convert.ToInt32(Console.ReadLine());
-            //return int.Parse(Console.ReadLine());
         }
         static void Main(string[] args)
         {
-            OpcoesMenuPrincipal opcaoDigitada = OpcoesMenuPrincipal.Sair;
+            OpcoesMenuPrincipal opcaoDigitada =
+                OpcoesMenuPrincipal.Sair;
 
-            //     ------Caso for int-----
-            //int opcaoDigitada = 5;
             do
             {
                 opcaoDigitada = Menu();
@@ -55,18 +51,26 @@ namespace ConsoleView
                 switch (opcaoDigitada)
                 {
                     case OpcoesMenuPrincipal.CadastrarCliente:
-                        Cliente cli = CadastrarCliente();
-                        ExibirDadosDoCliente(cli);
+                        Cliente c = CadastrarCliente();
+
+                        ClienteController cc = new ClienteController();
+                        cc.SalvarCliente(c);
+
+                        ExibirDadosCliente(c);
                         break;
                     case OpcoesMenuPrincipal.PesquisarCliente:
                         PesquisarCliente();
                         break;
+
+                    case OpcoesMenuPrincipal.ListarClientesCadastrados:
+                        ListarTodosClientes();
+                        break;
                     case OpcoesMenuPrincipal.EditarCliente:
                         break;
-                    case OpcoesMenuPrincipal.ExluirCliente:
+                    case OpcoesMenuPrincipal.ExcluirCliente:
+                        ExcluirCliente();
                         break;
                     case OpcoesMenuPrincipal.LimparTela:
-                        LimparTela();
                         break;
                     case OpcoesMenuPrincipal.Sair:
                         break;
@@ -76,38 +80,52 @@ namespace ConsoleView
 
             } while (opcaoDigitada != OpcoesMenuPrincipal.Sair);
 
-
         }
 
-        //Metodo Cliente
+        private static void ExcluirCliente()
+        {
+            Console.WriteLine("Digite o id do cliente que deseja excluir: ");
+            int idCliente = int.Parse(Console.ReadLine());
+
+            ClienteController cc = new ClienteController();
+            cc.ExcluirCliente(idCliente);
+        }
+
+        // Metodos Cliente
         private static Cliente CadastrarCliente()
         {
             Cliente cli = new Cliente();
-            cli._Endereco = new Endereco();
 
             Console.Write("Digite o nome: ");
             cli.Nome = Console.ReadLine();
 
             Console.WriteLine();
 
-            Console.Write("Digite o CPF:  ");
+            Console.Write("Digite o cpf: ");
             cli.Cpf = Console.ReadLine();
 
-            Console.Write("Digite o seu endereço: ");
-            cli._Endereco.Rua = Console.ReadLine();
+            // ... Endereco
+            Endereco end = new Endereco();
+
+            Console.Write("Digite o nome da rua: ");
+            end.Rua = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Digite o numero: ");
+            end.Numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
 
             Console.Write("Digite o complemento: ");
-            cli._Endereco.Complemento = Console.ReadLine();
+            end.Complemento = Console.ReadLine();
 
-            Console.Write("Digite o número da sua casa: ");
-            cli._Endereco.Numero = int.Parse(Console.ReadLine());
-
+            cli._Endereco = end;
 
             return cli;
-
         }
 
-        private static Cliente PesquisarCliente()
+        private static void PesquisarCliente()
         {
             Console.WriteLine("Digite o nome do cliente: ");
             string nomeCliente = Console.ReadLine();
@@ -116,28 +134,16 @@ namespace ConsoleView
             Cliente cli = cc.PesquisarPorNome(nomeCliente);
 
             if (cli != null)
-                ExibirDadosDoCliente(cli);
+                ExibirDadosCliente(cli);
             else
                 Console.WriteLine(" * Cliente não encontrado");
         }
 
-        private static Cliente EditarCliente()
-        {
-            return new Cliente();
-        }
-        private static Cliente ExcluirCliente()
-        {
-            return new Cliente();
-        }
-        private static void  LimparTela()
-        {
-            Console.Clear();
-        }
-        private static void ExibirDadosDoCliente(Cliente cliente)
+        private static void ExibirDadosCliente(Cliente cliente)
         {
             Console.WriteLine();
             Console.WriteLine("--- DADOS CLIENTE --- ");
-            Console.WriteLine("ID:" + cliente.PessoaID);
+            Console.WriteLine("ID:" + cliente.PessoaId);
             Console.WriteLine("Nome: " + cliente.Nome);
             Console.WriteLine("Cpf: " + cliente.Cpf);
 
@@ -166,14 +172,10 @@ namespace ConsoleView
             {
                 foreach (Cliente cli in lista)
                 {
-                    ExibirDadosDoCliente(cli);
+                    ExibirDadosCliente(cli);
                 }
             }
             Console.WriteLine();
         }
     }
 }
-
-
-
-
---
+++ SitemaEntregas/Controllers/EnderecoController.cs
@@ -0,0 +1,39 @@
+﻿using Modelos;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Controllers
+{
+    public class EnderecoController
+    {
+        static List<Endereco> MeusEnderecos = new List<Endereco>();
+        static int ultimoID = 0;
+
+        public void SalvarEnderco(Endereco endereco)
+        {
+            int id = ultimoID + 1;
+            ultimoID = id;
+
+            endereco.EnderecoID = id;
+            MeusEnderecos.Add(endereco);
+        }
+
+        public Endereco PesquisarPorRua(string nome)
+        {
+            var c = from x in MeusEnderecos
+                    where x.Rua.ToLower().Contains(nome.Trim().ToLower())
+                    select x;
+
+            if (c != null)
+                return c.FirstOrDefault();
+            else
+                return null;
+        }
+
+        public List<Endereco> ListarEnderecos()
+        {
+            return MeusEnderecos;
+        }
+    }
+}
+
